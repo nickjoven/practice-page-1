6c809194ca58d7ad2a21bc2b3313a288d5207514
Nicholas <nicholasjoven@gmail.com>
2022-07-11 10:13:20 -0400

    updated object pushing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 // append a set of images with text displaying over the images to the DOM
 
 let divArray = []
-let exampleAPI = 'https://www.reddit.com/r/all/top.json?raw_json=1&limit=5&t=day'
+let exampleAPI = 'https://www.reddit.com/r/all/top.json?raw_json=5&limit=5&t=day'
 
 // function to allow use of API url by passing arguments
 const customUrl = (subreddit = 'all', sort = 'top', limit = '10', time = 'day') => {
@@ -21,7 +21,15 @@ const fetchData = (url) => {
         while (divArray.length < 4) {
             for (let i = 1; i < res.data.children.length; i++) {
                 let newObj = {}
-                newObj.imgUrl = res.data.children[i].data["thumbnail_height"] ? res.data.children[i].data.preview.images[0].resolutions[1].url : ''
+                let nestArray = res.data.children[i].data
+                // title property - house textContent for h3 tag
+                newObj.title = nestArray.title
+                // subreddit property = house textContent for h4 tag
+                newObj.subreddit = nestArray.subreddit_name_prefixed
+                // upvotes property = house textContent for h4 tag
+                newObj.upvotes = nestArray.ups
+                // imgUrl property - house src for img tag
+                newObj.imgUrl = nestArray["thumbnail_height"] ? res.data.children[i].data.preview.images[0].resolutions[1].url : ''
                 divArray.push(newObj)
             }
         }
@@ -30,6 +38,9 @@ const fetchData = (url) => {
 
 fetchData(exampleAPI)
 
+
+
+// function to append images to div from array of objects with custom properties
 
 
 
